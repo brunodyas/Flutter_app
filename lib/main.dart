b24cfd3b60cecfe5a64fd707576d8f40a90c245c
Bruno Dias de Amorim <brunodyas@icloud.com>
2022-06-18 19:55:42 -0300

    Trabalhando com Imagens

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main(){
 
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
+    //debugShowCheckedModeBanner: false,
     title: "Frases do Dia",
     home: Container(
       margin: EdgeInsets.only(top: 40),
@@ -11,16 +11,10 @@ void main(){
       decoration: BoxDecoration(
         border: Border.all(width: 3, color: Colors.white)
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text("T1"),
-          Text("T2"),
-          Text("T3"),
-        ],
-
+      child: Image.asset(
+        "images/mesa.jpg",
+        fit: BoxFit.cover,
       ),
-    )
+    ),
   ));
 }
