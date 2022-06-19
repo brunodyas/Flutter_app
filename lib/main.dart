b69b24e7534ba176867b4420ef6da37571296d2b
Bruno Dias de Amorim <brunodyas@icloud.com>
2022-06-19 01:33:32 -0300

    Mudando o estado da aplicacao com o setState

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,64 @@ void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
 
-    home: Scaffold(
+    home: HomeStateFul(),
+  ));
+}
+
+class HomeStateFul extends StatefulWidget {
+  const HomeStateFul({Key? key}) : super(key: key);
+
+  @override
+  State<HomeStateFul> createState() => _HomeStateFulState();
+}
+
+class _HomeStateFulState extends State<HomeStateFul> {
+  var titulo = "Metafizer";
+  var texto1 = DateTime.now().toString();
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
       appBar: AppBar(
-        title: Text("Instagram"),
+        title: Text(titulo),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: (){
+                  setState(() {
+                    titulo = "Vai porra!";
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.amberAccent,
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                ),
+                child: Text("$titulo"),
+            ),
+            Text("$texto1")
+          ],
+        ),
+      ),
+
+
+    );
+  }
+}
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var titulo = "Metafizer";
+    var texto1 = DateTime.now().toString();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(titulo),
         backgroundColor: Colors.deepPurpleAccent,
       ),
       body: Padding(
@@ -20,25 +75,27 @@ void main(){
           padding: EdgeInsets.all(16),
           child: Row(
             children: <Widget>[
-              Text("Texto1",
+              Text(texto1,
                 style: TextStyle(
                   fontSize: 15,
                   fontStyle: FontStyle.normal,
                   fontWeight: FontWeight.bold,
                   color: Colors.lightGreenAccent,
+                  wordSpacing: 30,
                 ), ),
               Text("Texto2",
                 style: TextStyle(
                   fontSize: 15,
                   fontStyle: FontStyle.normal,
                   fontWeight: FontWeight.bold,
-
+                  color: Colors.lightGreenAccent,
+                  wordSpacing: 30,
                 ), ),
             ],
           ),
         ),
       ),
 
-    ),
-  ));
+    );
+  }
 }
